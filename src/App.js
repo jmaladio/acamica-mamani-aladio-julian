e8cd2cf7180e5935493558c08152ddccf113e1c4
Julián Aladio <jaladio@southernhorizon.tech>
2021-05-22 22:46:22 -0300

    #1 feat: setup ambientes env con URI y TOKEN Issue #1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 function App() {
   return (
     <div>
-
+      <h1>Hola Mundo</h1>
+      <ul>
+        <li>{process.env.REACT_APP_API_URI}</li>
+        <li>{process.env.REACT_APP_API_KEY}</li>
+      </ul>
     </div>
   );
 }
